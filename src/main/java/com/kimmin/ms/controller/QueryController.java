7bf27ba1930be47a12268ebfc046dcf66b73c3bc
Min Jin <t-mijin@microsoft.com>
2016-08-11 11:21:15 +0800

    New query interface
    Signed-off-by: Min Jin <t-mijin@microsoft.com>

--- src/main/java/com/kimmin/ms/controller/QueryController.java
+++ src/main/java/com/kimmin/ms/controller/QueryController.java
@@ -1,17 +1,21 @@
 package com.kimmin.ms.controller;
 
 import com.kimmin.ms.entity.Dish;
+import com.kimmin.ms.entity.Menu;
 import com.kimmin.ms.entity.User;
 import com.kimmin.ms.misc.Utils;
 import com.kimmin.ms.service.AuthService;
+import com.kimmin.ms.service.MenuService;
 import com.kimmin.ms.service.QueryService;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by t-mijin on 8/6/2016.
@@ -28,6 +32,9 @@ public class QueryController {
 
     @Autowired
     private QueryService queryService;
+
+    @Autowired
+    private MenuService menuService;
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
@@ -104,6 +111,33 @@ public class QueryController {
     public String queryLatestDishes(){
         try{
             return objectMapper.writeValueAsString(queryService.queryLatestDishes());
+        }catch (Throwable e){
+            e.printStackTrace();
+            return Utils.RESP_FAILURE;
+        }
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @RequestMapping(value = "/today/{id}", method = RequestMethod.GET)
+    public String queryUserToday(@PathVariable("id") String id){
+        try{
+            User user = authService.getUserById(id);
+            Map<String, Object> resp = new HashMap<String, Object>();
+            resp.put("upper", user.getUpper());
+            Set<Menu> menus = menuService.getMenuByUser(id);
+            Date today = new Date();
+            today.setHours(0);
+            today.setMinutes(0);
+            today.setSeconds(0);
+            boolean put = false;
+            for(Menu menu : menus){
+                if(menu.getDate().after(today)){
+                    resp.put("menu", menu);
+                    put = true;
+                }
+            }
+            return objectMapper.writeValueAsString(resp);
         }catch (Throwable e){
             e.printStackTrace();
             return Utils.RESP_FAILURE;
